f4de8c1c89055b43a38845e7ee1b994c74c2b089
Jared Grippe <jared@deadlyicon.com>
2010-02-03 16:48:16 -0800

    added print support for V8 and probably Ryhno

--- src/consolex.js
+++ src/consolex.js
@@ -19,10 +19,31 @@
   function emptyFunction(){}
 
   for (var i = 0; i < names.length; ++i){
-    window.console[names[i]] || (window.console[names[i]] = emptyFunction);
-    if (typeof window.console[names[i]] !== 'function')
-      window.console[names[i]] = (function(method) {
+    console[names[i]] || (console[names[i]] = emptyFunction);
+    if (typeof console[names[i]] !== 'function')
+      console[names[i]] = (function(method) {
           return function(){ return Function.prototype.apply.apply(method, [console,arguments]); };
-        })(window.console[names[i]]);
+        })(console[names[i]]);
   }
+  
+  // V8 and Rihno Support
+  if (typeof print === "function"){
+    if (console.log === emptyFunction) console.log = print;
+    
+    function print_with_prefix(prefix, args){
+      Array.prototype.unshift.call(args, prefix);
+      print.apply(this, args);
+    }
+    
+    if (console.info === emptyFunction) console.info = function(){
+      print_with_prefix('INFO:', arguments);
+    }
+    if (console.warn === emptyFunction) console.warn = function(){
+      print_with_prefix('WARN:', arguments);
+    }
+    if (console.error === emptyFunction) console.error = function(){
+      print_with_prefix('ERROR:', arguments);
+    }
+  }
+  
 })(this);
